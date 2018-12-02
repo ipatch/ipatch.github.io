f7d57834de343d330ba5a70d0ecd775b930016e3
Chris Jones <chris.r.jones.1983@gmail.com>
2018-12-01 19:18:37 -0600

    update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
+set -ex
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+echo -e "--------------------------------------------------"
+
 # Build the project.
+
+cd /opt/code/github/public/ipatch.github.io
+
+echo -e "--------------------------------------------------"
+
+pwd
+
+echo -e "--------------------------------------------------"
+
+cd ./src/
+
+pwd
+
+echo -e "--------------------------------------------------"
+
 hugo
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+mv ./build/* ../../
+
+echo -e "--------------------------------------------------"
+
+echo -e "moved build files from running 'hugo'" into project root
+
+echo -e "--------------------------------------------------"
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuilding site $(date)"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# When working with <USER>.github.io repos, the website is built from the master branch
+
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
+echo -e "--------------------------------------------------"
+echo -e " New build should be published at http://ipatch.github.io"
+echo -e "--------------------------------------------------"
